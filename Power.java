259fa66c49aaa7df863debd38823bf8dadfa6bd2
Buvana3112 <40532123+Buvana3112@users.noreply.github.com>
2019-01-13 17:13:24 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Power{
+    public static void main(String args[]){
+         Scanner s=new Scanner(System.in);
+         int x=s.nextInt();
+         int y=s.nextInt();
+         int z=1;
+         for(int i=0;i<y;i++){
+            z=z*x;
+            }
+         System.out.println(z);
+         }
+  }
