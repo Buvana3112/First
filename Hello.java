55b4c4799947e052996e55c19d0fb517383c372f
Buvana3112 <40532123+Buvana3112@users.noreply.github.com>
2019-01-13 09:38:43 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Hello{
+     public static void main(String args[]){
+          Scanner s=new Scanner(System.in);
+          int N=s.nextInt();
+          for(int i=0;i<N;i++){
+             System.out.println("Hello");
+             }
+          }
+    }
+          
