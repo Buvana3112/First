bed9fe5cdee78028cc892cd1f04f32a2a35ef7bd
Buvana3112 <40532123+Buvana3112@users.noreply.github.com>
2019-01-13 09:44:31 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Natural{
+     public static void main(String args[]){
+          Scanner s=new Scanner(System.in);
+          int n=s.nextInt();
+          int a=0;
+          for(int i=1;i<=n;i++){
+             a=a+i;
+             }
+          System.out.println(a);
+          }
+    }
