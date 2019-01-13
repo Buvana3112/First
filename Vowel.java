0f352b8084c1611ed0ab2bad2dcda225dabb8921
Buvana3112 <40532123+Buvana3112@users.noreply.github.com>
2019-01-13 09:01:09 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Vowel{
+     public static void main(String args[]){
+          Scanner s=new Scanner(System.in);
+          char ch=s.nextChar();
+          if( ch >= 'a' && ch <= 'z'){
+               if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+                    System.out.println("Vowel");
+               else
+                    System.out.println("Consonent");
+               }
+          else 
+               System.out.println("Invalid");
+         }
+    }
